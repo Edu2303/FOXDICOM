1fd458e5831e2a450d39283d8eebf0bd70e97cf0
Eduardo lourenço <ai.inteligencia.artificial.medica@gmail.com>
2022-11-16 18:09:58 -0300

    Project created

---
+++ backend/src/test/java/com/FOXDICOM/FOV/FovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FOXDICOM.FOV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
